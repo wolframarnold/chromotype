27f46c0483f62fda4496c2bcedbc8c2341edc2bc
Matthew McEachen <matthew+github@mceachen.org>
2011-05-17 12:45:05 -0700

    rails new from 3.1.0.beta1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photostructure::Application.config.secret_token = 'cdf605447da49e654414a4d067879affed61a126382b20749e5d557d5614235ea288c21f89bf4ab004ec7bb3ad21f3533b2bd86cc3be4fe7e065af5966e5a0c1'
