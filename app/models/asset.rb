9c99f6140eece3cacf57ad7ee280d44c9474b54a
Matthew McEachen <matthew+github@mceachen.org>
2011-11-23 21:09:38 -0800

    WIP

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,8 @@
 class Asset < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
+  belongs_to :directory
+
   scope :with_tag, lambda { |tag| includes(:tags).where("tags.id" => tag.id) }
 
   scope :with_tag_or_descendants, lambda { |tag| includes(:tags => [:ancestors]).where("ancestors_tags.id = ? or tags.id = ?", tag.id, tag.id) }
--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,8 +1,18 @@
 class Directory < ActiveRecord::Base
-  acts_as_tree #BOOM.
+  acts_as_tree :name_column => :basename, :dependent => :destroy
+  has_many :assets
+
+  def self.find_or_create_from_file(file)
+    # TODO find_or_create_by_path
+  end
 
   def self.process_mtime_deltas
     roots.each{|d| d.delay(:priority => d.priority).process if d.process?}
+  end
+
+  def path
+    # TODO: FINISH
+    # self_and_ancestors.reverse
   end
 
   def exists?
@@ -10,7 +20,15 @@ class Directory < ActiveRecord::Base
   end
 
   def process?
-    exists? and File.mtime(path) != last_mtime
+    !exists? or File.mtime(path) != last_mtime
+  end
+
+  def process process_all_assets = false
+    # Clone the assets that we know about before processing
+    if !exists?
+      parent.children.remove
+    end
+    # TODO: FINISH
   end
 
   def priority
