3dca6d3740563852b686ce8f98e00fd5921c7f5c
Matthew McEachen <matthew+github@mceachen.org>
2012-02-01 23:48:30 -0800

    added more test todos

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper.rb"
 
 describe Asset do
-  it "should delegate to proper Asset class methods to process a URI"
+  it "should assign or reuse an asset_uri with #uri="
+  it "should round-trip #uri= to #uri"
+  it "should return a normalized uri from #uri"
+  #maybe? it "should delegate to proper Asset class methods to process a URI"
+
 end
