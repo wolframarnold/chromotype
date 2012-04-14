21a251d16149b8ddabb1f7cb90e22aab47fccb6d
Matthew McEachen <matthew+github@mceachen.org>
2012-04-14 14:06:24 -0700

    cache the exif lookups

--- app/models/geo_tag.rb
+++ app/models/geo_tag.rb
@@ -1,6 +1,14 @@
 require 'geonames'
 
 class GeoTag < Tag
+  def self.cache
+    @@cache ||= begin
+      cache_dir = Rails.root + "cache"
+      cache_dir.mkpath
+      ActiveSupport::Cache.lookup_store(:file_store, cache_dir + "geo.cache")
+    end
+  end
+
   def self.root_name
     "where"
   end
@@ -15,11 +23,13 @@ class GeoTag < Tag
 
   def self.tag_for_lat_lon(lat, lon)
     return nil if lat.nil? || lon.nil?
-    places_nearby = Geonames::WebService.find_nearby_place_name(lat, lon) || []
-    nearest = places_nearby.first
-    return nil if nearest.nil? || nearest.geoname_id.to_i == 0
-    places = Geonames::WebService.hierarchy(nearest.geoname_id)
-    return nil if places.empty?
-    named_root.find_or_create_by_path(places.collect { |ea| ea.name })
+    place_path = cache.fetch("%.6f:%.6f" % [lat, lon], {:expires_in => 1.month}) do
+      places_nearby = Geonames::WebService.find_nearby_place_name(lat, lon) || []
+      nearest = places_nearby.first
+      return nil if nearest.nil? || nearest.geoname_id.to_i == 0
+      places = Geonames::WebService.hierarchy(nearest.geoname_id)
+      places.collect { |ea| ea.name }
+    end
+    named_root.find_or_create_by_path(place_path)
   end
 end
