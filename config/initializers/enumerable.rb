2c20f7ab4bfe376ea9b77b2acead90c81f1214a8
Matthew McEachen <matthew+github@mceachen.org>
2012-05-17 22:34:48 -0700

    added collect_hash

--- config/initializers/enumerable.rb
+++ config/initializers/enumerable.rb
@@ -2,4 +2,20 @@ module Enumerable
   def to_i
     map { |ea| ea.to_i }
   end unless public_method_defined? :to_i
+
+  def collect_hash(hash = Hash.new)
+    inject(hash) do |h, i|
+      y = yield(i)
+      if y.nil?
+        h
+      elsif y.respond_to? :merge
+        h.merge(y)
+      elsif y.respond_to?(:first) && y.respond_to?(:last)
+        h[y.first] = y.last
+        h
+      else
+        raise "Expected yield to return nil, a hash, or a 2 element array"
+      end
+    end
+  end
 end
---
+++ test/unit/enumerable_test.rb
@@ -0,0 +1,29 @@
+require "minitest_helper"
+
+describe Enumerable do
+  it "collects for array from hash results" do
+    [1, 2, 3].
+      collect_hash { |i| {i.to_s => i} }.
+      must_equal("1" => 1, "2" => 2, "3" => 3)
+  end
+  it "collects for array from array results" do
+    [:a, :b, :c].
+      collect_hash { |i| [i.to_s, i] }.
+      must_equal("a" => :a, "b" => :b, "c" => :c)
+  end
+  it "collects for hash from hash results" do
+    {:a => 1, :b => 2, :c => 3}.
+      collect_hash { |k, v| {k => v + 1} }.
+      must_equal(:a => 2, :b => 3, :c => 4)
+  end
+  it "collects for hash from array results" do
+    {:a => 1, :b => 2, :c => 3}.
+      collect_hash { |k, v| [k, v + 1] }.
+      must_equal(:a => 2, :b => 3, :c => 4)
+  end
+  it "skips nil yield results properly" do
+    {:a => 1, :b => 2, :c => 3, :d => 4}.
+      collect_hash { |k, v| [k, v] if v % 2 == 0 }.
+      must_equal(:b => 2, :d => 4)
+  end
+end
