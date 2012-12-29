c4c873df27eea53d01308270354a2a348cbccbce
Matthew McEachen <matthew+github@mceachen.org>
2012-12-29 12:32:49 -0800

    ""

--- test/unit/string_test.rb
+++ test/unit/string_test.rb
@@ -5,5 +5,8 @@ describe String do
     # http://en.wikipedia.org/wiki/SHA-1#Example_hashes
     "The quick brown fox jumps over the lazy dog".sha1.
       must_equal("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12")
+
+    "".sha1.
+      must_equal("da39a3ee5e6b4b0d3255bfef95601890afd80709")
   end
 end
