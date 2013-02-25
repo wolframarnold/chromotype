eebcd062395296a28edc9936d6382e3b365792c4
Matthew McEachen <matthew+github@mceachen.org>
2013-02-24 18:41:37 -0800

    add automatic flag to asset_tags

--- db/migrate/20110526005214_create_assets_tags.rb
+++ db/migrate/20110526005214_create_assets_tags.rb
@@ -3,6 +3,7 @@ class CreateAssetsTags < ActiveRecord::Migration
     create_table :asset_tags, :id => false do |t|
       t.references :asset, :required => true
       t.references :tag, :required => true
+      t.boolean :automatic
     end
     add_index :asset_tags, [:tag_id, :asset_id]
   end
