65c2c865176e8492f5425e5145346f5d7d26a860
Matthew McEachen <matthew+github@mceachen.org>
2013-02-24 18:41:16 -0800

    doc about secrets

--- config/initializers/y_secret_token.rb
+++ config/initializers/y_secret_token.rb
@@ -4,6 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+
+# This relies on an automatically generated Setting that's persisted in your RDBMS:
 ActiveSupport.migration_safe_on_load do
   Chromotype::Application.config.secret_token = Settings.secret_token
 end
