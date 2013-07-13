292a12d11b1931a4f2ff415d5792c578975730d8
Matthew McEachen <matthew+github@mceachen.org>
2013-07-13 00:20:28 -0700

    rot-6 to remove duplication

--- config/initializers/date.rb
+++ config/initializers/date.rb
@@ -3,14 +3,13 @@ class Date
     12 => :winter, 1 => :winter, 2 => :winter, 3 => :spring, 4 => :spring, 5 => :spring,
     6 => :summer, 7 => :summer, 8 => :summer, 9 => :autumn, 10 => :autumn, 11 => :autumn}
 
-  S_HEMISPHERE = {
-    6 => :winter, 7 => :winter, 8 => :winter, 9 => :spring, 10 => :spring, 11 => :spring,
-    12 => :summer, 1 => :summer, 2 => :summer, 3 => :autumn, 4 => :autumn, 5 => :autumn}
-
   # Returns :winter, :spring, :summer, or :autumn, using UK-based month-bound seasons.
   # See http://en.wikipedia.org/wiki/Winter
   def season
-    (Setting.is_northern_hemisphere ? N_HEMISPHERE : S_HEMISPHERE)[self.month]
+    m = self.month
+    # the -1 and +1 are to handle 1-indexed rot-6.
+    m = ((m - 1 + 6) % 12) + 1 unless Setting.is_northern_hemisphere
+    N_HEMISPHERE[m]
   end
 
   def month_name
